16b75f27e0cba23161e701d5cbceac7719151414
Hufeiya <a4188@qq.com>
2014-02-25 21:17:07 +0800

    add test.cpp as a test,hehe

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	cout<<"fuck\n";
+	return 0;
+}
